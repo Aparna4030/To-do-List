bf3a84b11163b7ee14d721fb91f74ad8bea6baa6
Your Name <aparnanambiampurath245@gmail.com>
2023-05-24 23:08:25 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const[title,setTitle]=useState("")
+  const[description,setDescription]=useState("")
+  const handleTitle = (event) => {
+    // Function Scope
+    console.log(event.target.value)
+    setTitle(event.target.value)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <h1>To do list</h1>
+    <div className='d-flex justify-content-center'>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control onChange={handleTitle} type="text" placeholder="Title" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="Description" placeholder="Description" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
     </div>
+   </div>
   );
 }
 
